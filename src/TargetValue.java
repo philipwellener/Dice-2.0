4321e04948a32fe423c1596590105c9ce84768cb
Philip <Philip@philips-mbp.fios-router.home>
2018-10-14 21:18:52 -0400

    Yeet

--- src/TargetValue.java
+++ src/TargetValue.java
@@ -15,17 +15,37 @@ public class TargetValue
 		
 		Dice die1 = new Dice();
 		Dice die2 = new Dice();
+		int target = 0;
+		boolean valid = true;
+		while (valid)
+		{
+			System.out.print("Choose a target value for the sum of two dice rolls: ");
+			int target1 =in.nextInt();
+			if (target1 >= 2)
+			{
+				if (target1 <= 12)
+				{
+					target = target1;
+					valid = false;
+				}
+				else
+					System.out.println("That is not a valid target value");
+					
+			}
+			else 
+				System.out.println("That is not a valid target value");
 		
+		}
+
 		boolean guess = true;
 		while (guess) 
 		{
-			System.out.print("Choose a target value for the sum of two dice rolls: ");
-			int target =in.nextInt();
+
 			int num1 = die1.roll();
 			int num2 = die2.roll();
 			if (num1+num2 != target) 
 			{
-				System.out.println("You rolled a " + num1 + " and a "+ num2 + " which equals " + (num1 + num2) + ". Try again.");
+				System.out.println("You rolled a " + num1 + " and a "+ num2 + " which equals " + (num1 + num2) + ".");
 			}
 			else if (num1+num2 == target)
 			{
